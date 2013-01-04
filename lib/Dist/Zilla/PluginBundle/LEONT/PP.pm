8959c1a3618929ac255a1693ce53328a24757cef
Leon Timmermans <fawaka@gmail.com>
2013-01-04 00:23:01 +0000

    Import of LEONT/Dist-Zilla-PluginBundle-LEONT-0.001 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-LEONT
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         LEONT/Dist-Zilla-PluginBundle-LEONT-0.001.tar.gz
    gitpan-cpan-author:       LEONT
    gitpan-cpan-maturity:     released

---
+++ lib/Dist/Zilla/PluginBundle/LEONT/PP.pm
@@ -0,0 +1,42 @@
+package Dist::Zilla::PluginBundle::LEONT::PP;
+{
+  $Dist::Zilla::PluginBundle::LEONT::PP::VERSION = '0.001';
+}
+
+use Moose;
+
+extends 'Dist::Zilla::PluginBundle::LEONT';
+
+has '+install_tool' => (
+	default => 'eumm',
+);
+
+1;
+
+# ABSTRACT: Legacy plugin bundle for pure-perl modules
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::PluginBundle::LEONT::PP - Legacy plugin bundle for pure-perl modules
+
+=head1 VERSION
+
+version 0.001
+
+=head1 AUTHOR
+
+Leon Timmermans <leont@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Leon Timmermans.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ lib/Dist/Zilla/PluginBundle/LEONT/XS.pm
@@ -0,0 +1,42 @@
+package Dist::Zilla::PluginBundle::LEONT::XS;
+{
+  $Dist::Zilla::PluginBundle::LEONT::XS::VERSION = '0.001';
+}
+
+use Moose;
+
+extends 'Dist::Zilla::PluginBundle::LEONT';
+
+has '+install_tool' => (
+	default => 'mb',
+);
+
+1;
+
+# ABSTRACT: Legacy plugin bundle for xs modules
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::PluginBundle::LEONT::XS - Legacy plugin bundle for xs modules
+
+=head1 VERSION
+
+version 0.001
+
+=head1 AUTHOR
+
+Leon Timmermans <leont@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Leon Timmermans.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
